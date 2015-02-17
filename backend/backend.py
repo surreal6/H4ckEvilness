c52b723a662d63875cce45f1cf9149a6da92270d
Victor Perez <viperey@gmail.com>
2015-02-17 15:45:40 +0100

    Email Cleaning

--- backend/backend.py
+++ backend/backend.py
@@ -1,16 +1,24 @@
-from flask import Flask
+from flask import Flask, make_response, render_template, request
 from functions import generate_email_hash
 
 app = Flask(__name__)
 
 
-@app.route('/email/<emailInput>/')
+@app.route('/emailDeprecated/<emailInput>/')
 def email(emailInput):
-    # TODO. Check db
     print(generate_email_hash(emailInput))
-    # TODO. Return url with 301 and url @Location.
     return 'Correaco! '+emailInput
 
+
+@app.route('/email/')
+def email2():
+    # TODO. Check db
+    # TODO. Return url with 201 and url @Location.
+    email_in = request.args.get('email')
+    hash = generate_email_hash(email_in)
+    resp = make_response('202', 202)
+    resp.headers['Location'] = '/url/'+hash+"/"
+    return resp
 
 @app.route('/')
 def hello_world():
